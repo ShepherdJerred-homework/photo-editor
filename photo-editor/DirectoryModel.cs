0323bff3e42f8d9e3cd65968d328d9ebf50f1161
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-09-17 09:48:26 -0500

    Add DirectoryModal

---
+++ photo-editor/DirectoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace photo_editor {
+    class DirectoryModel {
+        public DirectoryInfo directory { get; set; }
+        FileInfo[] getImagesInDirectory() {
+            FileInfo[] files = directory.GetFiles("*.jpg");
+            return files;
+        }
+    }
+}
